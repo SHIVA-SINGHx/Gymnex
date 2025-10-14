ef85e3588fc84917cbaf7b6c372f48fa7b1d9bb0
SHIVA-SINGHx <shivasingh.1dev@gmail.com>
2025-10-14 10:49:26 -0700

    In Navbar file added header and logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,79 @@
-import React from 'react'
+"use client"
+
+import React from "react";
+import Link from "next/link";
+import { Activity, DumbbellIcon, HomeIcon, UserIcon } from 'lucide-react';
+import { SignInButton, SignUpButton, UserButton, useUser } from "@clerk/nextjs";
+import { Button } from "./ui/button";
 
 const Navbar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+    const {isSignedIn} = useUser();
 
-export default Navbar
+  return (
+    <header className="fixed top-0 left-0 right-0 z-50 bg-background/60 backdrop-blur-md border-b border-border py-3">
+      <div className="container mx-auto flex items-center justify-between">
+        
+        {/* logo */}
+        <Link href="/" className="flex items-center gap-2">
+          <div className="p-1 bg-primary/10 rounded">
+            <Activity className="w-4 h-4 text-primary" />
+          </div>
+          <span className="text-xl font-bold font-mono">
+            Gym<span className="text-primary">nex</span>
+          </span>
+        </Link>
+
+        {/* Navigation */}
+        <nav className="flex items-center gap-5">
+            { isSignedIn ? (
+                <>
+             <Link
+                href="/"
+                className="flex items-center gap-1.5 text-sm hover:text-primary transition-colors"
+              >
+                <HomeIcon size={16} />
+                <span>Home</span>
+              </Link>
+
+              <Link
+                href="/generate-program"
+                className="flex items-center gap-1.5 text-sm hover:text-primary transition-colors"
+              >
+                <DumbbellIcon size={16} />
+                <span>Generate</span>
+              </Link>
+
+              <Link
+                href="/profile"
+                className="flex items-center gap-1.5 text-sm hover:text-primary transition-colors"
+              >
+                <UserIcon size={16} />
+                <span>Profile</span>
+              </Link>
+
+              <Button
+              asChild
+              variant={"outline"}
+              className="ml-2 border-primary/50 text-primary hover:text-white hover:bg-primary/10"
+              />
+              <Link href={"/program-generate"}>Get Started</Link>
+              <UserButton/>
+         
+                </>
+            ) : (
+                <>
+                <div>
+                    <h1>hello </h1>
+                </div>
+                </>
+            )}
+
+        </nav>
+        
+
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
