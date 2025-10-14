eae79f4379424662951db72dd24ec5a1548e285d
SHIVA-SINGHx <shivasingh.1dev@gmail.com>
2025-10-14 11:42:10 -0700

    major changes in HomePage section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,10 +36,8 @@ const HomePage = () => {
               <div className="h-px w-full bg-gradient-to-r from-primary via-secondary to-primary opacity-50"></div>
 
               <p className="text-xl text-muted-foreground w-2/3">
-                {/* Talk to our AI assistant and get personalized diet plans and workout routines
-                designed just for you */}
               Chat with our AI trainer to get personalized workout routines
-               and diet plans tailored just for you.
+              and diet plans tailored just for you.
               </p>
 
               {/* STATS */}
@@ -84,12 +82,39 @@ const HomePage = () => {
                 <div className="absolute bottom-0 left-0 w-16 h-16 border-l-2 border-b-2 border-border" />
                 <div className="absolute bottom-0 right-0 w-16 h-16 border-r-2 border-b-2 border-border" />
               </div>
+
+              {/* Image-Container */}
+              <div className="relative aspect-square max-w-lg mx-auto">
+                <div className="relative overflow-hidden rounded-lg bg-cyber-black">
+                  <img 
+                  src="/main-ai.png"
+                  alt="AI Fitness"
+                  className="size-full object-cover object-center" />
+
+                  {/* Scan-line */}
+                    <div className="absolute inset-0 bg-[linear-gradient(transparent_0%,transparent_calc(50%-1px),var(--cyber-glow-primary)_50%,transparent_calc(50%+1px),transparent_100%)] bg-[length:100%_8px] animate-scanline pointer-events-none" />
+
+                    <div className="absolute inset-0 pointer-events-none">
+                    <div className="absolute top-1/3 left-1/3 w-1/3 h-1/3 border border-primary/40 rounded-full" />
+
+                    {/* Targeting lines */}
+                    <div className="absolute top-1/2 left-0 w-1/4 h-px bg-primary/50" />
+                    <div className="absolute top-1/2 right-0 w-1/4 h-px bg-primary/50" />
+                    <div className="absolute top-0 left-1/2 h-1/4 w-px bg-primary/50" />
+                    <div className="absolute bottom-0 left-1/2 h-1/4 w-px bg-primary/50" />
+                  </div>
+
+                  <div className="absolute inset-0 bg-gradient-to-t from-background via-background/40 to-transparent" />
+                </div>
+
+
+                </div>
+              </div>
               
 
               </div>
             </div>
             </div>
-          </div>
       </section>
     </div>
   );
