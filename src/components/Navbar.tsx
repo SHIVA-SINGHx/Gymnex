241dc41ba8aeb118ca3bec2204ca52878d17f4e6
SHIVA-SINGHx <shivasingh.1dev@gmail.com>
2025-10-14 10:58:06 -0700

    Fixes Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,17 @@
-"use client"
+"use client";
 
 import React from "react";
 import Link from "next/link";
-import { Activity, DumbbellIcon, HomeIcon, UserIcon } from 'lucide-react';
+import { Activity, DumbbellIcon, HomeIcon, UserIcon } from "lucide-react";
 import { SignInButton, SignUpButton, UserButton, useUser } from "@clerk/nextjs";
 import { Button } from "./ui/button";
 
 const Navbar = () => {
-    const {isSignedIn} = useUser();
+  const { isSignedIn } = useUser();
 
   return (
     <header className="fixed top-0 left-0 right-0 z-50 bg-background/60 backdrop-blur-md border-b border-border py-3">
       <div className="container mx-auto flex items-center justify-between">
-        
         {/* logo */}
         <Link href="/" className="flex items-center gap-2">
           <div className="p-1 bg-primary/10 rounded">
@@ -25,9 +24,9 @@ const Navbar = () => {
 
         {/* Navigation */}
         <nav className="flex items-center gap-5">
-            { isSignedIn ? (
-                <>
-             <Link
+          {isSignedIn ? (
+            <>
+              <Link
                 href="/"
                 className="flex items-center gap-1.5 text-sm hover:text-primary transition-colors"
               >
@@ -52,25 +51,33 @@ const Navbar = () => {
               </Link>
 
               <Button
-              asChild
-              variant={"outline"}
-              className="ml-2 border-primary/50 text-primary hover:text-white hover:bg-primary/10"
-              />
-              <Link href={"/program-generate"}>Get Started</Link>
-              <UserButton/>
-         
-                </>
-            ) : (
-                <>
-                <div>
-                    <h1>hello </h1>
-                </div>
-                </>
-            )}
+                asChild
+                variant="outline"
+                className="ml-2 border-primary/50 text-primary hover:text-white hover:bg-primary/10"
+              >
+                <Link href="/generate-program">Get Started</Link>
+              </Button>
+              <UserButton />
+            </>
+          ) : (
+            <>
+              <SignInButton>
+                <Button
+                  variant={"outline"}
+                  className="border-primary/50 text-primary hover:text-white hover:bg-primary/10"
+                >
+                  Sign In
+                </Button>
+              </SignInButton>
 
+              <SignUpButton>
+                <Button className="bg-primary text-primary-foreground hover:bg-primary/90">
+                  Sign Up
+                </Button>
+              </SignUpButton>
+            </>
+          )}
         </nav>
-        
-
       </div>
     </header>
   );
